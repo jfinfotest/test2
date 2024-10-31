3fb1b340eb7cf232342b90e23e33811509436f39
jfinfotest <jfinfotest@gmail.com>
2024-10-31 16:15:46 -0500

    Update test_assessment.py

--- test_assessment.py
+++ test_assessment.py
@@ -1,12 +1,17 @@
 import unittest
+import assessment  # Asegúrate de que assessment.py exista y sea importable
 
 class TestAssessment(unittest.TestCase):
-            def test_suma(self):
-                self.assertEqual(namespace['suma'](2, 2), 4)
-                self.assertEqual(namespace['suma'](-1, 1), 0)
-                self.assertEqual(namespace['suma'](10, 5), 15)
-            
-            def test_resta(self):
-                self.assertEqual(namespace['resta'](5, 3), 2)
-                self.assertEqual(namespace['resta'](10, 2), 8)
-                self.assertEqual(namespace['resta'](-5, 2), -7)
+    def test_suma(self):
+        self.assertEqual(assessment.suma(2, 2), 4)
+        self.assertEqual(assessment.suma(-1, 1), 0)
+        self.assertEqual(assessment.suma(10, 5), 15)
+
+    def test_resta(self):
+        self.assertEqual(assessment.resta(5, 3), 2)
+        self.assertEqual(assessment.resta(10, 2), 8)
+        self.assertEqual(assessment.resta(-5, 2), -7)
+
+# Esto es crucial para que unittest funcione.  De lo contrario, no se ejecutará nada.
+if __name__ == '__main__':
+    unittest.main()
